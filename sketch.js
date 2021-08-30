6e6a4197bab731f322ce0441243646f14f82e721
Mark <89092701+Mark2021-GitHub@users.noreply.github.com>
2021-08-31 01:46:17 +0900

    원소 설명 추가

--- sketch.js
+++ sketch.js
@@ -10,6 +10,9 @@ var inputTxt = document.querySelector(".txt");
 var playNextBtton = document.getElementById("playNext");
 var radioEn = document.getElementById("en");
 var bSwitch = document.getElementById("check1");
+var dtText = document.getElementById("dt1");
+var ddText = document.getElementById("dd1");
+
 
 // for speechSynthesis;
 var synth = window.speechSynthesis;
@@ -20,15 +23,67 @@ let rateSlider;
 let enVoice = [];
 let kVoice = [];
 let bVoice = false;
-
 let next = 1 ;
 
+class Element {
+  constructor(id, s, ename, kname, x, y, desc) {
+    this.id = id;
+    this.s = s;
+    this.ename = ename;
+    this.kname = kname;
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.tsize = 15;
+    this.str=" "+this.id +" ["+this.s +"] "+this.ename+" ("+this.kname+")";
+    this.desc = desc;
+  }
+  
+  show(mx, my) {
+    stroke(255);
+    if (this.contains(mx, my)) {
+      fill(0, 255, 0);
+      rect(this.x, this.y, this.w, this.h);
+      fill(255, 0, 0);
+      dtText.innerText =  this.str;
+      ddText.innerText = this.desc;
+    } else {
+      fill(200);
+      rect(this.x, this.y, this.w, this.h);
+      fill(0, 0, 255);
+    }
+    textSize(this.tsize);
+    text(this.str,this.x,this.y + this.tsize);
+  }
+
+  contains(mx, my) {
+    if (
+      this.x < mx &&
+      mx < this.x + this.w &&
+      this.y < my &&
+      my < this.y + this.h
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  speech(lang, vid){
+    if(lang == 'ko-KR'){
+      textToSpeech(this.kname, vid); 
+    } else {
+      textToSpeech(this.ename, vid);  
+    }
+  }
+}
+
+
 function setup() {
-  cnv = createCanvas(300, 700);
+  cnv = createCanvas(300, 800);
   
   setupVoices();
-  
-  setupElements();
+  setupElement();
   
 }
 
@@ -48,12 +103,15 @@ let isNext = false;
 
 function playNext(){
   if(synth.speaking) {
-    return;
+   return;
   }
   
   let sel = voiceSelect.value();
   let str = splitTokens(sel, ':');
   let vnumber = Number(str[0]);
+  
+    dtText.innerText =  elements[next].str;
+    ddText.innerText = elements[next].desc;
   
    if(bSwitch.checked == true){
        if(isNext == false ){
@@ -210,187 +268,129 @@ function mousePressed() {
   }
 }
 
-class Element {
-  constructor(id, s, ename, kname, x, y) {
-    this.id = id;
-    this.s = s;
-    this.ename = ename;
-    this.kname = kname;
-    this.x = x;
-    this.y = y;
-    this.w = w;
-    this.h = h;
-    this.tsize = 15;
-   
-  }
+function setupElement() {
+  elements[1] = new Element(1, "H", "Hydrogen", "수소", x, y, ": 가장 작고 가벼운 원소");
+  y += h;
+  n++;
+  elements[2] = new Element(2, "He", "Helium", "헬륨", x, y, ": 의외로 지구에서는 희귀한 원소");
+  y += h;
+  n++;
+  elements[3] = new Element(3, "Li", "Lithium", "리튬", x, y, ": 가장 가벼운 금속원소");
+  y += h;
+  n++;
+  elements[4] = new Element(4, "Be", "Beryllium", "베릴륨", x, y, ": 알루미늄보다 가볍고 강철보다 강한 금속");
+  y += h;
+  n++;
+  elements[5] = new Element(5, "B", "Boron", "붕소", x, y, ": ");
+  y += h;
+  n++;
+  elements[6] = new Element(6, "C", "Carbon", "탄소", x, y, ": ");
+  y += h;
+  n++;
+  elements[7] = new Element(7, "N", "Nitrogen", "질소", x, y, ": ");
+  y += h;
+  n++;
+  elements[8] = new Element(8, "O", "Oxygen", "산소", x, y, ": ");
+  y += h;
+  n++;
+  elements[9] = new Element(9, "F", "Fluorine","플루오린/불소",x,y, ": ");
+  y += h;
+  n++;
+  elements[10] = new Element(10, "Ne", "Neon", "네온", x, y, ": ");
+  y += h;
+  n++;
+  elements[11] = new Element(11,"Na","Sodium / Natrium","소듐/나트륨",x,y, ": ");
+  y += h;
+  n++;
+  elements[12] = new Element(12,"Mg","Magnesium","마그네슘",x,y, ": ");
+  y += h;
+  n++;
+  elements[13] = new Element(13,"Al","Aluminium","알루미늄",x,y, ": ");
+  y += h;
+  n++;
+  elements[14] = new Element(14,"Si","Silicon","규소/실리콘",x,y, ": ");
+  y += h;
+  n++;
+  elements[15] = new Element(15, "P", "Phosphorus", "인", x, y, ": ");
+  y += h;
+  n++;
+  elements[16] = new Element(16, "S", "Sulfur", "황", x, y, ": ");
+  y += h;
+  n++;
+  elements[17] = new Element(17, "Cl", "Chlorine", "염소", x, y, ": ");
+  y += h;
+  n++;
+  elements[18] = new Element(18, "Ar", "Argon", "아르곤", x, y, ": ");
+  y += h;
+  n++;
+  elements[19] = new Element(19, "K","Potassium / Kalium","포타슘/칼륨",x,y, ": ");
+  y += h;
+  n++;
+  elements[20] = new Element(20, "Ca", "Calcium", "칼슘", x, y, ": ");
+  y += h;
+  n++;
+  elements[21] = new Element(21, "Sc", "Scandium", "스칸듐", x, y, ": ");
+  y += h;
+  n++;
+  elements[22] = new Element(22, "Ti", "Titanium", "타이타늄", x, y, ": ");
+  y += h;
+  n++;
+  elements[23] = new Element(23, "V", "Vanadium", "바나듐", x, y, ": ");
+  y += h;
+  n++;
+  elements[24] = new Element(24, "Cr", "Chromium", "크로뮴", x, y, ": ");
+  y += h;
+  n++;
   
-  show(mx, my) {
-    stroke(255);
-    if (this.contains(mx, my)) {
-      fill(0, 255, 0);
-      rect(this.x, this.y, this.w, this.h);
-
-      textSize(this.tsize);
-      fill(255, 0, 0);
-      text(
-        " " +
-          this.id +
-          " [" +
-          this.s +
-          "] " +
-          this.ename +
-          " (" +
-          this.kname +
-          ")",
-        this.x,
-        this.y + this.tsize
-      );
-    } else {
-      fill(200);
-      rect(this.x, this.y, this.w, this.h);
-
-      textSize(this.tsize);
-      fill(0, 0, 255);
-      text(
-        " " +
-          this.id +
-          " [" +
-          "  " +
-          "] " +
-          this.ename +
-          " (" +
-          "         " +
-          ")",
-        this.x,
-        this.y + this.tsize
-      );
-    }
-  }
-
-  contains(mx, my) {
-    if (
-      this.x < mx &&
-      mx < this.x + this.w &&
-      this.y < my &&
-      my < this.y + this.h
-    ) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-  speech(lang, vid){
-    if(lang == 'ko-KR'){
-      textToSpeech(this.kname, vid); 
-    } else {
-      textToSpeech(this.ename, vid);  
-    }
-  }
-}
-
-function setupElements() {
-  elements[1] = new Element(1, "H", "Hydrogen", "수소", x, y);
+  elements[25] = new Element(25,"Mn","Manganese","망가니즈/망간",x,y, ": ");
   y += h;
   n++;
-  elements[2] = new Element(2, "He", "Helium", "헬륨", x, y);
+  elements[26] = new Element(26, "Fe", "Iron / Ferrum", "철", x, y, ": ");
   y += h;
   n++;
-  elements[3] = new Element(3, "Li", "Lithium", "리튬", x, y);
+ elements[27] = new Element(27, "Co", "Cobalt", "코발트", x, y, ": ");
   y += h;
   n++;
-  elements[4] = new Element(4, "Be", "Beryllium", "베릴륨", x, y);
+  elements[28] = new Element(28, "Ni", "Nickel", "니켈", x, y, ": ");
   y += h;
   n++;
-  elements[5] = new Element(5, "B", "Boron", "붕소", x, y);
-  y += h;
-  n++;
-  elements[6] = new Element(6, "C", "Carbon", "탄소", x, y);
-  y += h;
-  n++;
-  elements[7] = new Element(7, "N", "Nitrogen", "질소", x, y);
-  y += h;
-  n++;
-  elements[8] = new Element(8, "O", "Oxygen", "산소", x, y);
-  y += h;
-  n++;
-  elements[9] = new Element(9, "F", "Fluorine","플루오린/불소",x,y);
-  y += h;
-  n++;
-  elements[10] = new Element(10, "Ne", "Neon", "네온", x, y);
-  y += h;
-  n++;
-  elements[11] = new Element(11,"Na","Sodium / Natrium","소듐/나트륨",x,y);
-  y += h;
-  n++;
-  elements[12] = new Element(12,"Mg","Magnesium","마그네슘",x,y);
-  y += h;
-  n++;
-  elements[13] = new Element(13,"Al","Aluminium","알루미늄",x,y);
-  y += h;
-  n++;
-  elements[14] = new Element(14,"Si","Silicon","규소/실리콘",x,y);
-  y += h;
-  n++;
-  elements[15] = new Element(15, "P", "Phosphorus", "인", x, y);
-  y += h;
-  n++;
-  elements[16] = new Element(16, "S", "Sulfur", "황", x, y);
-  y += h;
-  n++;
-  elements[17] = new Element(17, "Cl", "Chlorine", "염소", x, y);
-  y += h;
-  n++;
-  elements[18] = new Element(18, "Ar", "Argon", "아르곤", x, y);
-  y += h;
-  n++;
-  elements[19] = new Element(19, "K","Potassium / Kalium","포타슘/칼륨",x,y);
-  y += h;
-  n++;
-  elements[20] = new Element(20, "Ca", "Calcium", "칼슘", x, y);
-  y += h;
-  n++;
-  elements[n] = new Element(25,"Mn","Manganese","망가니즈/망간",x,y);
-  y += h;
-  n++;
-  elements[n] = new Element(26, "Fe", "Iron / Ferrum", "철", x, y);
-  y += h;
-  n++;
-  elements[n] = new Element(29,"Cu","Copper / Cuprum","구리",x,y);
+ 
+  elements[29] = new Element(29,"Cu","Copper / Cuprum","구리",x,y, ": ");
   y += h;
   n++;
 
-  elements[n] = new Element(30, "Zn", "Zinc", "아연", x, y);
+  elements[30] = new Element(30, "Zn", "Zinc", "아연", x, y, ": ");
   y += h;
   n++;
 
-  elements[n] = new Element(38,"Sr","Strontium","스트론튬",x,y);
+  elements[n] = new Element(38,"Sr","Strontium","스트론튬",x,y, ": ");
   y += h;
   n++;
 
-  elements[n] = new Element(47, "Ag", "Silver / Argentum", "은", x, y);
+  elements[n] = new Element(47, "Ag", "Silver / Argentum", "은", x, y, ": ");
   y += h;
   n++;
-  elements[n] = new Element(53, "I", "Iodine", "아이오딘/요오드", x, y);
-  y += h;
-  n++;
-
-  elements[n] = new Element(56, "Ba", "Barium", "바륨", x, y);
+  elements[n] = new Element(53, "I", "Iodine", "아이오딘/요오드", x, y, ": ");
   y += h;
   n++;
 
-  elements[n] = new Element(78, "Pt", "Platinum", "백금/플래티넘", x, y);
+  elements[n] = new Element(56, "Ba", "Barium", "바륨", x, y, ": ");
   y += h;
   n++;
 
-  elements[n] = new Element(79, "Au", "Gold / Aurum", "금", x, y);
+  elements[n] = new Element(78, "Pt", "Platinum", "백금/플래티넘", x, y, ": ");
   y += h;
   n++;
 
-  elements[n] = new Element(80,"Hg","Mercury / Hydrargyrum","수은",x,y);
+  elements[n] = new Element(79, "Au", "Gold / Aurum", "금", x, y, ": ");
   y += h;
   n++;
 
-  elements[n] = new Element(82, "Pb", "Lead / Plumbum", "납", x, y);
+  elements[n] = new Element(80,"Hg","Mercury / Hydrargyrum","수은",x,y, ": ");
+  y += h;
+  n++;
+
+  elements[n] = new Element(82, "Pb", "Lead / Plumbum", "납", x, y, ": ");
   y += h;
   n++;
 }
